2533ddd49701959270b84fe1d2cf90171e40a274
bigmanik <igbinomwanhia10@gmail.com>
2023-03-15 02:36:15 -0700

    style updates

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function toggleBookmark() {
     const bookmarkButton = document.getElementById('bookmark-button');
+    const bookmarkIcon = document.getElementById('circle-id');
+    const bookmarkIcons = document.getElementById('path-id');
     
 
     
@@ -7,18 +9,30 @@ function toggleBookmark() {
       bookmarkButton.classList.remove('active');
       bookmarkButton.style.color = ' black';
       bookmarkButton.textContent = 'Bookmark';
+      bookmarkIcon.style.fill = '';
+      bookmarkIcons.style.fill = '';
+
     } else {
       bookmarkButton.classList.add('active');
       bookmarkButton.style.color = ' hsl(176, 72%, 28%)';
       bookmarkButton.textContent = 'Bookmarked';
+      bookmarkIcon.style.fill = "hsl(176, 72%, 28%)";
+      bookmarkIcons.style.fill = "hsl(0, 0%, 100%)";
+
       
     }
   }
   
-//   const bookmarkButton = document.getElementById('bookmark-button');
-//   const mySVG = document.getElementById('my-svg');
-// bookmarkButton.addEventListener('click', function() {
-//   mySVG.setAttribute('fill', 'red');
-// });
+  // const radioButtons = document.querySelectorAll('.radio-button');
+
+  // radioButtons.forEach(button => {
+  //   button.addEventListener('click', () => {
+  //     radioButtons.forEach(btn => {
+  //       btn.classList.remove('selected');
+  //     });
+  //     button.classList.add('selected');
+  //   });
+  // });
+  
 
   
